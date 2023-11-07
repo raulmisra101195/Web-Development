60cf96d60b58ca161694342b7d7d7d8a6c5c8140
raulmisra101195 <148179042+raulmisra101195@users.noreply.github.com>
2023-11-07 15:02:52 +0530

    Add files via upload

---
+++ 104. ReactJS Intro/hello.js
@@ -0,0 +1,1 @@
+console.log("Hello Everyone we are learning ReactJS")
---
+++ 104. ReactJS Intro/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Hello Nandan How are you</h1>
+  </React.StrictMode>,
+  document.getElementById('anil')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
